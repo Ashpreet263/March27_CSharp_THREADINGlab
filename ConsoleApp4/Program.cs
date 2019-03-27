e8c46fb0e13e0889909cf3f0b055f955825d11f8
visualstudio <visualstudio@122-PC33>
2019-03-27 10:47:58 -0400

    commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace ConsoleApp4
+
+namespace ThreadingWorkshop
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Download();
+
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            // ensure git commit is done!
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                Console.WriteLine("Complete");
+            });
         }
     }
 }
